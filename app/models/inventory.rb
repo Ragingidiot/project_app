117655ac6ee710e8589a916da0eaef231cbc45c5
Scott Russell <your.email@example.com>
2016-03-16 04:18:54 +0000

    Final Update

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,11 @@
 class Inventory < ActiveRecord::Base
+    before_save
+    validates :gametitle, presence: true,
+        length: { maximum: 50 }
+        
+    validates :gametype, presence: true,
+        length: { maximum: 50 }
+        
+    validates :gamerating, presence: true,
+        length: { maximum: 50 }
 end
